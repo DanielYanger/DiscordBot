1cd866bd70705340d41666e83810b256c9228007
Daniel Yang <danielyang2002@gmail.com>
2020-05-17 21:40:35 -0500

    Testing embeding

--- bot.py
+++ bot.py
@@ -8,4 +8,31 @@ client = commands.Bot(command_prefix='!')
 async def on_ready():
     print('Bot is ready.')
 
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server.')
+
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server.')
+
+
+@client.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+
+@client.command()
+async def display(ctx, arg1, arg2):
+    channel = ctx.message.channel
+    embed = discord.Embed(
+        title=arg1,
+        description=arg2,
+        colour=discord.Color.green()
+    )
+    await channel.send(embed=embed)
+
+
 client.run('NzExMzg5Nzc1ODQ4MDEzODQ1.XsFw3w.24LsNuW7p1bORtMxSb-MHY7OpZw')
