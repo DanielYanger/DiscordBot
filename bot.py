88cb507d9acd004ded17b22f57989c1b310fd3ea
Daniel Yang <danielyang2002@gmail.com>
2020-05-18 20:26:25 -0500

    Continued messing with embeds

--- bot.py
+++ bot.py
@@ -1,22 +1,22 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix="!")
 
 
 @client.event
 async def on_ready():
-    print('Bot is ready.')
+    print("Bot is ready.")
 
 
 @client.event
 async def on_member_join(member):
-    print(f'{member} has joined the server.')
+    print(f"{member} has joined the server.")
 
 
 @client.event
 async def on_member_remove(member):
-    print(f'{member} has left the server.')
+    print(f"{member} has left the server.")
 
 
 @client.command()
@@ -25,14 +25,14 @@ async def test(ctx, arg):
 
 
 @client.command()
-async def display(ctx, arg1, arg2):
+async def ban(ctx, user, arg1):
     channel = ctx.message.channel
     embed = discord.Embed(
-        title=arg1,
-        description=arg2,
-        colour=discord.Color.green()
+        title="User Banned",
+        description=user + " was banned for " + arg1,
+        colour=discord.Color.green(),
     )
     await channel.send(embed=embed)
 
 
-client.run('NzExMzg5Nzc1ODQ4MDEzODQ1.XsFw3w.24LsNuW7p1bORtMxSb-MHY7OpZw')
+client.run("NzExMzg5Nzc1ODQ4MDEzODQ1.XsFw3w.24LsNuW7p1bORtMxSb-MHY7OpZw")
